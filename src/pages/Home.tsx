238af6590dc50bf7f1e9e5821405da833f9cab4a
João Marcos <joaomarcos8922@gmail.comm>
2022-11-29 22:08:40 -0300

    netlify url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ export function Home(){
 const [data, setData] = useState([]);
 
 useEffect(() =>{
-fetch('http://localhost:3000/static/project.json', {
+fetch('https://joaomarcos.netlify.app/static/project.json', {
 
 }).then((response) => response.json())
 .then(setData)
